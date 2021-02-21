f683bac43cc758babfe39ffbfdadc60f91fc4471
Diego Rubio <darubiomunoz@gmail.com>
2021-02-21 09:47:09 -0300

    Modifications in the component styles

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -14,13 +14,22 @@ const ProfileCard = () => {
                         <img src={Pattern} className="card-img-top" alt="Pattern as the profile's banner" />
                         <img src={UserPhoto} className="position-absolute card-img-user img-thumbnail rounded-circle w-25" alt="User's profile photo" />
                         <div className="card-body border-bottom d-flex">
-                            <h2 className="card-title fw-bold">Victor Crest <span className="card-age fs-3">26</span></h2>
-                            <p className="card-location">London</p>
+                            <h2 className="card-title display-5">Victor Crest <span className="card-age h2">26</span></h2>
+                            <p className="card-location h4">London</p>
                         </div>
-                        <div className="card-stats">
-                            <p className="card-followers">Follower</p>
-                            <p className="card-likes">Likes</p>
-                            <p className="card-photos">Photos</p>
+                        <div className="card-stats row">
+                            <div className="col d-flex">
+                                <p className="card-figure h3">80k</p>
+                                <small className="card-followers h5">Followers</small>
+                            </div>
+                            <div className="col d-flex">
+                                <p className="card-figure h3">803k</p>
+                                <small className="card-likes h5">Likes</small>
+                            </div>
+                            <div className="col d-flex">
+                                <p className="card-figure h3">1.4k</p>
+                                <small className="card-photos h5">Photos</small>
+                            </div>
                         </div>
                     </div>
                 </div>
