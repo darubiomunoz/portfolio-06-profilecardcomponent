e7e6ab0c081e6f70860c1cf22962f1f24d2efb9b
Diego Rubio <darubiomunoz@gmail.com>
2021-02-21 10:57:43 -0300

    background styles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
+import Top from './assets/images/bg-pattern-top.svg';
+import Bottom from './assets/images/bg-pattern-bottom.svg';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <img src={Top} className="pattern-top position-absolute" alt="Top design pattern" />
+    <img src={Bottom} className="pattern-bottom position-absolute" alt="Bottom design pattern" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
