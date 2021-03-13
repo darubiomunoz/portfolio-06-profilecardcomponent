353bc6b12f5bcb7a685adcfe89ef24d4498ed21c
Diego Rubio <darubiomunoz@gmail.com>
2021-03-13 05:53:13 -0300

    tabindex name correction

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -12,23 +12,23 @@ const ProfileCard = () => {
                 <div className="column">
                     <div className="card position-relative">
                         <img src={Pattern} className="card-img-top" alt="Pattern as profile's banner" />
-                        <img src={UserPhoto} className="position-absolute card-img-user img-thumbnail rounded-circle w-25" tabindex="0" alt="User's profile photo" />
+                        <img src={UserPhoto} className="position-absolute card-img-user img-thumbnail rounded-circle w-25" tabIndex="0" alt="User's profile photo" />
                         <div className="card-body border-bottom d-flex">
-                            <h2 className="card-title display-5" tabindex="0">Victor Crest <span className="card-age h2">26</span></h2>
-                            <p className="card-location h4" tabindex="0">London</p>
+                            <h2 className="card-title display-5" tabIndex="0">Victor Crest <span className="card-age h2">26</span></h2>
+                            <p className="card-location h4" tabIndex="0">London</p>
                         </div>
                         <div className="card-stats row">
                             <div className="col d-flex">
-                                <p className="card-figure h3" tabindex="0" aria-label="80.000">80k</p>
-                                <small className="card-followers h5" tabindex="0">Followers</small>
+                                <p className="card-figure h3" tabIndex="0" aria-label="80.000">80k</p>
+                                <small className="card-followers h5" tabIndex="0">Followers</small>
                             </div>
                             <div className="col d-flex">
-                                <p className="card-figure h3" tabindex="0" aria-label="803.000">803k</p>
-                                <small className="card-likes h5" tabindex="0">Likes</small>
+                                <p className="card-figure h3" tabIndex="0" aria-label="803.000">803k</p>
+                                <small className="card-likes h5" tabIndex="0">Likes</small>
                             </div>
                             <div className="col d-flex">
-                                <p className="card-figure h3" tabindex="0" aria-label="1.400">1.4k</p>
-                                <small className="card-photos h5" tabindex="0">Photos</small>
+                                <p className="card-figure h3" tabIndex="0" aria-label="1.400">1.4k</p>
+                                <small className="card-photos h5" tabIndex="0">Photos</small>
                             </div>
                         </div>
                     </div>
